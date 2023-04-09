4f0432795bedd90ebf541d5aa1159fb9b542a151
Amrendra <amrendra1111singh@gmail.com>
2023-04-09 15:59:20 +0530

    added successfully

---
+++ footer.php
@@ -0,0 +1,115 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>Document</title>
+</head>
+<body>
+ 
+
+<footer class="footer-section">
+        <div class="container">
+            <div class="footer-cta pt-5 pb-5">
+                <div class="row">
+                    <div class="col-xl-4 col-md-4 mb-30">
+                        <div class="single-cta">
+                            <i class="fas fa-map-marker-alt"></i>
+                            <div class="cta-text">
+                                <h4>Find us</h4>
+                                <span>155 monega road, London</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-md-4 mb-30">
+                        <div class="single-cta">
+                            <i class="fas fa-phone"></i>
+                            <div class="cta-text">
+                                <h4>Call us</h4>
+                                <span>0700000000</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-md-4 mb-30" >
+                        <div class="single-cta">
+                            <i class="far fa-envelope-open"></i>
+                            <div class="cta-text">
+                                <h4>Mail us</h4>
+                                <span>malvikajagdish@gmail.com</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-content pt-5 pb-5">
+                <div class="row">
+                    <div class="col-xl-4 col-lg-4 mb-50">
+                        <div class="footer-widget">
+                            <div class="footer-logo">
+                                <a href="index.html"><img src="images/logo.png" class="img-fluid" alt="logo" style ="width: 100px;"></a>
+                            </div>
+                            <div class="footer-text">
+                                <p>Lorem ipsum dolor sit amet, consec tetur adipisicing elit, sed do eiusmod tempor incididuntut consec tetur adipisicing
+                                elit,Lorem ipsum dolor sit amet.</p>
+                            </div>
+                           
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-4 col-md-6 mb-30">
+                        <div class="footer-widget">
+                            <div class="footer-widget-heading">
+                                <h3>Useful Links</h3>
+                            </div>
+                            <ul>
+                                <li><a href="#"> <abbr title="under construction">home</abbr> </a></li>
+                                <li><a href="#">How to Return?</a></li>
+                                <li><a href="#">Refund Policy</a></li>                  
+                                <li><a href="#">Meet the Team</a></li>
+                                <!-- <li><a href="#">Expert Team</a></li> -->
+                               
+                                <!-- <li><a href="#">Latest News</a></li> -->
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-4 col-md-6 mb-50">
+                        <div class="footer-widget">
+                            <div class="footer-widget-heading">
+                                <h3>Subscribe</h3>
+                            </div>
+                            <div class="footer-text mb-25" >
+                                <p>Don’t miss to subscribe to our new feeds, kindly enter your email id below.</p>
+                            </div>
+                            <div class="subscribe-form" id ="contact-us">
+                                <form action="#">
+                                    <input type="text" placeholder="Email Address">
+                                    <button>send</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="copyright-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-6 col-lg-6 text-center text-lg-left">
+                        <div class="copyright-text" >
+                            <p>Copyright &copy; 2018, All Right Reserved <a href="main.php">Opus Corporation</a></p>
+                        </div>
+                    </div>
+                   
+                </div>
+            </div>
+        </div>
+    </footer>
+  
+
+</footer>
+
+  <script src="index.js"></script>
+ 
+</body>
+</html>
---
+++ main.php
@@ -0,0 +1,118 @@
+<?php include("header.php");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <link rel ="stylesheet" href =style.css>
+ <title>Opus corporation</title>
+</head>
+<body>
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="./images/image2.jpg" class="carousel-img" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Donate to our Christmas Appel</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="./images/image3.jpg" class="carousel-img" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Christmas Jumper Day 16th December</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="./images/image1.jpg." class="carousel-img" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Merry Christmas</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+<h1 class ="heading">Featured Products</h1>
+
+  <div class="wrapper">
+  
+        <div class="card">
+        <img src="./images/card1.jpg" class="card-img" alt="">
+        <div class="info">
+                <h1>Christmas Cards</h1>
+                <p>Lorem Ipsum is simply dummy text from the printing and typeseting industry</p><button>Checkout</button>
+            </div>
+</div>
+
+       <div class="card">
+       <img src="./images/card2.jpg" class="card-img" alt="">
+       <div class="info">
+                <h1>Miniature Pine Trees</h1>
+                <p>Lorem Ipsum is simply dummy text from the printing and typeseting industry</p><button>Checkout</button>
+            </div>
+</div>
+
+       <div class="card">
+       <img src="./images/card3.jpg" class="card-img" alt="">
+       <div class="info">
+                <h1>Gift Cards</h1>
+                <p>Lorem Ipsum is simply dummy text from the printing and typeseting industry</p><button>Checkout</button>
+            </div>
+</div>
+
+       <div class="card">
+       <img src="./images/card4.jpg" class="card-img" alt="">
+       <div class="info">
+                <h1>Merry&Bright</h1>
+                <p>Lorem Ipsum is simply dummy text from the printing and typeseting industry</p><button>Checkout</button>
+            </div>
+     
+</div>
+
+
+</div>
+</div>
+<h1 class ="heading">OUR STORY</h1> <br>
+<div class = "about" >
+<p id ="test" class ="paragraph"><span>Opus Corporation</span> is London based charity organisation that has been in existence for more
+than three decades and still occupies half of the same ground floor in the old fire station
+building. In the hay days when the founder –Dr. Fredrick was alive; this main office was the
+venue of many meetings and innovative ideas. Seeing the place now, nobody will believe that
+the first corporative Community Bank and Association in the Southeast region were founded
+here. Those were days when many of their clients were younger and middle-aged people not
+retired and senior citizens as it is presently.</p> <br>
+<p class ="paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis delectus, magnam cupiditate hic nihil culpa quas accusantium dolore ipsum similique perspiciatis molestias necessitatibus vero cum explicabo, cumque assumenda? Veniam, nulla dolorem numquam illum aut veritatis mollitia itaque quis doloremque quas quisquam perferendis, animi praesentium vitae et consectetur sunt autem ducimus incidunt nemo illo dolore! Repellat debitis, voluptatem, necessitatibus accusamus ratione obcaecati quas iure iusto optio ut deserunt, molestias quos labore eius sint facere earum! Eligendi aspernatur deleniti, beatae quos adipisci voluptate corporis asperiores voluptatibus quas ratione eum illum est ipsa soluta dolore tempora qui quis rerum laudantium officia modi possimus dolorem enim consectetur. Obcaecati blanditiis doloribus possimus debitis quo quaerat veritatis ratione amet molestias ea nostrum similique distinctio dicta vel nobis expedita quam, dignissimos iure sunt odit provident dolore fugiat cupiditate facere.</p> <br>
+<p id="about-us" class ="paragraph">Sit amet consectetur adipisicing elit. Quis delectus, magnam cupiditate hic nihil culpa quas accusantium dolore ipsum similique perspiciatis molestias necessitatibus vero cum explicabo, cumque assumenda? Veniam, nulla dolorem numquam illum aut veritatis mollitia itaque quis doloremque quas quisquam perferendis, animi praesentium vitae et consectetur sunt autem ducimus incidunt nemo illo dolore! Repellat debitis, voluptatem, necessitatibus accusamus ratione obcaecati quas iure iusto optio ut deserunt, molestias quos labore eius sint facere earum! Eligendi aspernatur deleniti, beatae quos adipisci voluptate corporis asperiores voluptatibus quas ratione eum illum est ipsa soluta dolore tempora qui quis rerum laudantium officia modi possimus dolorem enim consectetur. Obcaecati blanditiis doloribus possimus debitis quo quaerat veritatis ratione amet molestias ea nostrum similique distinctio dicta vel nobis expedita quam, dignissimos iure sunt odit provident dolore fugiat cupiditate facere.</p>
+
+
+</div>
+<?php include("footer.php"); ?>
+
+
+
+
+
+
+       
+
+  <script src="index.js"></script>
+ 
+</body>
+</html>
